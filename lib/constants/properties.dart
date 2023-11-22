50abb608daf8c4db5e2b2b611a4cd6116c1cce21
ZRIOUIL Issam <izriouil@gmail.com>
2023-11-22 23:06:31 +0100

    Add layout, properties, and theme constants

---
+++ lib/constants/properties.dart
@@ -0,0 +1,2 @@
+const kDefaultAvatarImage =
+    "https://assets-global.website-files.com/609ccf5d994ce738b4c92672/63bd0c52ba8d916ba95fd074_new-logo.svg";
---
+++ lib/constants/theme_constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kBrandColor = Color(0xFF6158CB);
+const kBackgroundColor = Color(0xFFFFFFFF);
+const kBackgroundVariantColor = Color(0xFFEAECF8);
+const kTextColor = Color(0xFF101B37);
