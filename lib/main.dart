31bc9dd5a633549e6af11e10208135c9929f4a87
ZRIOUIL Issam <izriouil@gmail.com>
2023-11-23 17:25:10 +0100

    Add Flutter Bloc for authentication (Not used, just to fix some regression)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:valiq_editor_demo/config/firebase_config.dart';
 import 'package:valiq_editor_demo/config/service_locator.dart';
+import 'package:valiq_editor_demo/editor/cubits/auth/auth.cubit.dart';
 import 'package:valiq_editor_demo/editor/valiq-editor.app.dart';
 
 void main() {
@@ -16,8 +18,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ValiqEditorApplication(),
+    return BlocProvider(
+      create: (context) => AuthCubit(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Valiq Editor Demo | APEXIVE',
+        restorationScopeId: 'valiq-editor-demo',
+        home: ValiqEditorApplication(),
+      ),
     );
   }
 }
