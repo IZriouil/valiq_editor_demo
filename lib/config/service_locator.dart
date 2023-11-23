a1b3b392d7158b3cd25e8e19e329583f49d3227f
ZRIOUIL Issam <izriouil@gmail.com>
2023-11-23 17:24:12 +0100

    Add AuthenticationService to service locator

--- lib/config/service_locator.dart
+++ lib/config/service_locator.dart
@@ -2,6 +2,7 @@
 // Path: lib\config\service_locator.dart
 import 'package:get_it/get_it.dart';
 import 'package:valiq_editor_demo/services/account.service.dart';
+import 'package:valiq_editor_demo/services/authentication.service.dart';
 import 'package:valiq_editor_demo/services/category.service.dart';
 import 'package:valiq_editor_demo/services/item.service.dart';
 import 'package:valiq_editor_demo/services/menu.service.dart';
@@ -10,7 +11,7 @@ final locator = GetIt.instance;
 
 void setupLocator() {
   // locator.registerLazySingleton(() => StorageService());
-
+  locator.registerLazySingleton(() => AuthenticationService());
   locator.registerLazySingleton(() => AccountService());
   locator.registerLazySingleton(() => MenuService());
   locator.registerLazySingleton(() => CategoryService());
